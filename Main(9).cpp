16e5b459455a223b201ff1970d1c9d94fac46053
Shubhampaithankar23 <sandippaithankar73@gmail.com>
2024-08-26 15:44:04 +0530

    Main(9).cpp

---
+++ Main(9).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+	int number;
+	//prompt user for input
+	cout<< "Enter a number:";
+	cin>> number;
+	
+	//check if the number is positive or negative 
+	if(number>=0)
+	{
+		cout<< " The number " <<  number <<" is positive.\n";
+	}
+	else{
+		cout<< " The number " << number << " is negative.\n";
+	}
+	return 0;
+}
